3bea0c21f11f3318d50ece2f307ee4b45d8c7768
Garrett Estrin <garrett.estrin@gmail.com>
2018-02-04 19:46:09 -0800

    initial commit

---
+++ wp-content/themes/twentyseventeen/index.php
@@ -0,0 +1,13 @@
+<!-- Custom HTML by Garrett Estrin | GarrettEstrin.com -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>AshleyEstrin.com</title>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+  </head>
+  <body style="background-color: #c7ced1;">
+    <h1 style="color: black; text-align: center; font-family: 'Quicksand', sans-serif; margin-top: 25%; font-size: 72px;">coming soon</h1>
+    <h1 style="color: black; text-align: center; font-family: 'Quicksand', sans-serif; margin-top: 5%; font-size: 36px;">xo, ash</h1>
+  </body>
+</html>
